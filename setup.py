f852781230043954d2974403074e334a10d654a8
Leonardo Fedalto <lfedalto@gmail.com>
2017-09-05 13:51:53 -0300

    Add mypy hook

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pre-commit-hooks',
+    version='0.1',
+    url='https://github.com/Fedalto/pre-commit-hooks',
+    license='MIT',
+    author='Leonardo Fedalto',
+    author_email='lfedalto@gmail.com',
+    description='pre-commit hooks',
+
+    install_requires=[
+        'mypy'
+    ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
